2acd36a4a981cc3b41ccd9f298003a7c6b7169b1
ooleksander <d2345e678n9@yandex.ru>
2024-06-07 19:13:47 +0500

    added deploy config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {images: {
-    domains: ['nozhtopor.na4u.ru'],
-  },};
-
-export default nextConfig;
+const nextConfig = {
+  images: {
+    exportTrailingSlash: true,
+    domains: ["nozhtopor.na4u.ru"],
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId }
+    ) {
+      return {
+        "/": { page: "/" },
+        "/CartPage": {page:"/CartPage"},
+        "/Favourite": {page:"/Favourite"},
+        "/Manufacturer": {page: "/Manufacturer"},
+        "/News":{page: "/News"},
+        "/ProductCard":{page:"/ProductCard"},
+        "/Search":{page:"/Search"},
+        // добавьте другие пути, которые нужно экспортировать
+      };
+    },
+  },
+};
